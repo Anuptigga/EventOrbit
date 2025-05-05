e627eb6f20773f154afacdfe480b273ce3a18cf9
Kumar Anshuman <kanshuman6484@gmail.com>
2025-05-05 22:15:13 +0530

    frontend initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-2xl font-bold bg-green-800 text-white p-2">
+        Event Orbit
+      </h1>
+    </>
+  )
+}
+
+export default App
